ca1578313170f769a975f39162ea99ca8158ce6d
Łukasz Godziejewski <lgodziejewski@gmail.com>
2020-12-05 09:23:30 +0100

    Implement day 5 part 1

---
+++ lib/day-5/logic.ex
@@ -0,0 +1,58 @@
+defmodule Advent.Day5 do
+  @day_no "5"
+
+  def run do
+    IO.puts("Starting day #{@day_no} logic\n")
+
+    input = ReadInput.read_input("lib/day-#{@day_no}/input")
+
+    # input = ReadInput.read_input("lib/day-#{@day_no}/test-input")
+
+    boarding_passes = parse_input(input)
+
+    part1(boarding_passes)
+    part2(boarding_passes)
+
+    IO.puts("\nDay #{@day_no} logic finished")
+  end
+
+  defp part1(input) do
+    IO.puts("\n Part 1:\n")
+
+    result = Enum.max_by(input, fn {_, _, seat_id} -> seat_id end)
+
+    IO.inspect(result)
+  end
+
+  defp part2(input) do
+    IO.puts("\n Part 2:\n")
+  end
+
+  # possible values: FB, LR
+  # B -> 1
+  # F -> 0
+  # R -> 1
+  # L -> 0
+  defp parse_input(input) do
+    input
+    |> Enum.map(fn entry ->
+      {row, col} = String.split_at(entry, -3)
+
+      row_no =
+        row
+        |> String.replace("F", "0")
+        |> String.replace("B", "1")
+        |> String.to_integer(2)
+
+      col_no =
+        col
+        |> String.replace("R", "1")
+        |> String.replace("L", "0")
+        |> String.to_integer(2)
+
+      seat_id = row_no * 8 + col_no
+
+      {row_no, col_no, seat_id}
+    end)
+  end
+end
---
+++ lib/day-5/script.exs
@@ -0,0 +1,1 @@
+Advent.Day5.run()
