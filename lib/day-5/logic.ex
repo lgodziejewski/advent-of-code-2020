ddbc069979d465a31e2c1b978670bb8dd800f187
Łukasz Godziejewski <lgodziejewski@gmail.com>
2020-12-05 09:53:58 +0100

    Implement day 5 part 2

--- lib/day-5/logic.ex
+++ lib/day-5/logic.ex
@@ -26,6 +26,37 @@ defmodule Advent.Day5 do
 
   defp part2(input) do
     IO.puts("\n Part 2:\n")
+
+    seat_ids = input |> Enum.map(fn {_, _, seat_id} -> seat_id end) |> Enum.sort()
+
+    seat_ids
+    |> binary_search(true)
+    |> IO.inspect(label: "Result")
+  end
+
+  defp binary_search(list = [val], true) when length(list) == 1, do: val - 1
+  defp binary_search(list = [val], false) when length(list) == 1, do: val + 1
+
+  defp binary_search(list, _) do
+    seats_count = length(list)
+    [offset | _] = list
+
+    middle = div(seats_count, 2)
+
+    value = Enum.at(list, middle)
+    offset_value = value - offset
+
+    is_further? = offset_value == middle
+
+    {list_start, list_end} = Enum.split(list, middle)
+
+    new_list =
+      case is_further? do
+        true -> list_end
+        false -> list_start
+      end
+
+    binary_search(new_list, is_further?)
   end
 
   # possible values: FB, LR
