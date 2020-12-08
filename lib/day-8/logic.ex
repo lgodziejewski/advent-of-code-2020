09320b2b54c5e54cf19acfa7f03c74d1c6bd4415
Łukasz Godziejewski <lgodziejewski@gmail.com>
2020-12-08 09:38:12 +0100

    Implement day 8 part 2

--- lib/day-8/logic.ex
+++ lib/day-8/logic.ex
@@ -15,14 +15,39 @@ defmodule Advent.Day8 do
     IO.puts("\nDay #{@day_no} logic finished")
   end
 
-  defp part1(input) do
+  defp part1(instruction_map) do
     IO.puts("\n Part 1:\n")
 
-    invoke_instruction(input, 0, 0) |> IO.inspect(label: "Result")
+    invoke_instruction(instruction_map, 0, 0) |> IO.inspect(label: "Result")
   end
 
-  defp part2(input) do
+  defp part2(instruction_map) do
     IO.puts("\n Part 2:\n")
+
+    try_next_change(instruction_map, 0, {:start, 0}) |> IO.inspect(label: "Result")
+  end
+
+  defp try_next_change(_, _, {:finished, result}), do: result
+
+  defp try_next_change(orig_instr_map, new_pos, _) do
+    %{instruction: instr} = Map.get(orig_instr_map, new_pos)
+
+    new_instr =
+      case instr do
+        "jmp" -> "nop"
+        "nop" -> "jmp"
+        val -> val
+      end
+
+    updated_map = put_in(orig_instr_map, [new_pos, :instruction], new_instr)
+    res = invoke_instruction(updated_map, 0, 0)
+
+    case res do
+      {:finished, _} -> IO.inspect(new_pos, label: "Incorrect entry position")
+      _ -> nil
+    end
+
+    try_next_change(orig_instr_map, new_pos + 1, res)
   end
 
   def parse_input(input) do
@@ -39,21 +64,27 @@ defmodule Advent.Day8 do
   end
 
   defp invoke_instruction(instruction_map, position, result) do
-    %{instruction: instr, value: value, count: count} = Map.get(instruction_map, position)
+    entry = Map.get(instruction_map, position)
 
-    if count > 0 do
-      result
+    if is_nil(entry) do
+      {:finished, result}
     else
-      instruction_map = put_in(instruction_map, [position, :count], count + 1)
+      %{instruction: instr, value: value, count: count} = entry
 
-      {new_position, new_result} =
-        case instr do
-          "acc" -> {position + 1, result + value}
-          "nop" -> {position + 1, result}
-          "jmp" -> {position + value, result}
-        end
+      if count > 0 do
+        {:loop, result}
+      else
+        instruction_map = put_in(instruction_map, [position, :count], count + 1)
 
-      invoke_instruction(instruction_map, new_position, new_result)
+        {new_position, new_result} =
+          case instr do
+            "acc" -> {position + 1, result + value}
+            "nop" -> {position + 1, result}
+            "jmp" -> {position + value, result}
+          end
+
+        invoke_instruction(instruction_map, new_position, new_result)
+      end
     end
   end
 end
