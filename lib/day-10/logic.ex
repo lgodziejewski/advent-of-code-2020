899c30a38db284759e8b37fb97f82fe68de1ec39
Łukasz Godziejewski <lgodziejewski@gmail.com>
2020-12-10 08:11:17 +0100

    Implement day 10 part 2 - reduce implementation

--- lib/day-10/logic.ex
+++ lib/day-10/logic.ex
@@ -13,7 +13,7 @@ defmodule Advent.Day10 do
     sorted_list = Enum.sort(input)
 
     part1(sorted_list)
-    part2(input)
+    part2(sorted_list)
 
     IO.puts("\nDay #{@day_no} logic finished")
   end
@@ -38,5 +38,52 @@ defmodule Advent.Day10 do
 
   defp part2(input) do
     IO.puts("\n Part 2:\n")
+
+    list = [0] ++ input
+
+    {time, _} =
+      :timer.tc(fn ->
+        part2_reduce(list)
+      end)
+
+    IO.inspect(time, label: "reduce time [us]")
+  end
+
+  defp part2_reduce(input) do
+    {scores, _} =
+      input
+      |> Enum.reduce({%{}, []}, fn el, acc ->
+        {scores, previous_three} = acc
+
+        score =
+          previous_three
+          |> Enum.map(&{&1, el - &1})
+          |> Enum.filter(fn {_, diff} -> diff <= 3 end)
+          |> Enum.map(fn {el, _} -> Map.get(scores, el, 0) end)
+          |> Enum.reduce(0, &(&1 + &2))
+          |> normalize_score()
+
+        new_previous = new_previous(previous_three, el)
+        new_scores = put_in(scores, [el], score)
+
+        {new_scores, new_previous}
+      end)
+
+    max = Enum.max(input)
+
+    IO.inspect(Map.get(scores, max), label: "Result reduce")
+  end
+
+  defp normalize_score(0), do: 1
+  defp normalize_score(score), do: score
+
+  defp new_previous([], el), do: [el]
+
+  defp new_previous([_skip, second, third], el) do
+    new_previous([second, third], el)
+  end
+
+  defp new_previous(previous, el) do
+    previous ++ [el]
   end
 end
