5e032f968a16385d6845c9169fae94833a1a7fc8
Łukasz Godziejewski <lgodziejewski@gmail.com>
2020-12-14 20:55:08 +0100

    Implement day 14 part 2

--- lib/day-14/logic.ex
+++ lib/day-14/logic.ex
@@ -6,7 +6,7 @@ defmodule Advent.Day14 do
   def run do
     IO.puts("Starting day #{@day_no} logic\n")
 
-    # ReadInput.read_input("lib/day-#{@day_no}/test-input")
+    # ReadInput.read_input("lib/day-#{@day_no}/test-input-2")
     input =
       ReadInput.read_input("lib/day-#{@day_no}/input")
       |> parse_input()
@@ -25,8 +25,9 @@ defmodule Advent.Day14 do
       if cmd == "mask" do
         {cmd, value_str}
       else
-        [_, memory_address] = Regex.run(~r/mem\[(\d+)\]/, cmd)
+        [_, memory_address_str] = Regex.run(~r/mem\[(\d+)\]/, cmd)
         value = String.to_integer(value_str)
+        memory_address = String.to_integer(memory_address_str)
         {memory_address, value}
       end
     end)
@@ -38,44 +39,78 @@ defmodule Advent.Day14 do
     initial_state = {%{}, ""}
 
     Enum.reduce(input, initial_state, fn {cmd, val}, {state, mask} ->
-      apply_command(cmd, val, state, mask)
+      apply_command_part1(cmd, val, state, mask)
     end)
     |> calc_result()
     |> IO.inspect(label: "Result")
   end
 
-  defp apply_command("mask", val, state, _) do
-    and_mask =
-      String.replace(val, "X", "1")
-      |> String.to_integer(2)
-
-    or_mask =
-      String.replace(val, "X", "0")
-      |> String.to_integer(2)
+  defp apply_command_part1("mask", val, state, _) do
+    and_mask = String.replace(val, "X", "1") |> String.to_integer(2)
+    or_mask = String.replace(val, "X", "0") |> String.to_integer(2)
 
     masks = {and_mask, or_mask}
     {state, masks}
   end
 
-  defp apply_command(address, val, state, masks) do
+  defp apply_command_part1(address, val, state, masks) do
     {and_mask, or_mask} = masks
 
-    value_after_mask =
-      val
-      |> band(and_mask)
-      |> bor(or_mask)
+    value_after_mask = val |> band(and_mask) |> bor(or_mask)
 
     new_state = Map.put(state, address, value_after_mask)
     {new_state, masks}
+  end
+
+  defp part2(input) do
+    IO.puts("\n Part 2:\n")
+
+    initial_state = {%{}, ""}
+
+    Enum.reduce(input, initial_state, fn {cmd, val}, {state, mask} ->
+      apply_command_part2(cmd, val, state, mask)
+    end)
+    |> calc_result()
+    |> IO.inspect(label: "Result")
+  end
+
+  defp apply_command_part2("mask", val, state, _), do: {state, val}
+
+  defp apply_command_part2(address, val, state, mask) do
+    new_state =
+      mask
+      |> apply_mask(address)
+      |> Enum.reduce(state, fn adr, current_state ->
+        Map.put(current_state, adr, val)
+      end)
+
+    {new_state, mask}
+  end
+
+  defp apply_mask(mask, address) do
+    and_mask =
+      mask |> String.replace("0", "1") |> String.replace("X", "0") |> String.to_integer(2)
+
+    or_masks = get_mask_variants(mask)
+    mid_address = band(address, and_mask)
+
+    or_masks
+    |> Enum.map(fn or_mask -> bor(mid_address, or_mask) end)
+  end
+
+  defp get_mask_variants(mask) do
+    if String.contains?(mask, "X") do
+      mask1 = String.replace(mask, "X", "1", global: false)
+      mask2 = String.replace(mask, "X", "0", global: false)
+      get_mask_variants(mask1) ++ get_mask_variants(mask2)
+    else
+      [String.to_integer(mask, 2)]
+    end
   end
 
   defp calc_result({state, _}) do
     state
     |> Map.values()
     |> Enum.reduce(0, fn el, acc -> acc + el end)
-  end
-
-  defp part2(input) do
-    IO.puts("\n Part 2:\n")
   end
 end
